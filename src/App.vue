390ad597c46c8ded7543aaf381ed7d3026e933be
evinsdesign <kelvinsaputra024@gmail.com>
2024-03-31 14:55:08 +0800

    feat: NavbarComponent

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,8 @@ import { RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+* {
+  font-family: 'Montserrat', sans-serif;
+}
+</style>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,68 @@
+<template>
+  <Disclosure as="nav" class="bg-darkBlue" v-slot="{ open }">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-16 items-center justify-center">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <DisclosureButton
+            class="relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-gray-700 hover:text-orangePastel focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orangePastel"
+          >
+            <span class="absolute -inset-0.5"></span>
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+        <div class="flex flex-shrink-0 items-center">
+          <logo class="h-14 w-auto" />
+        </div>
+        <div class="flex items-center justify-center sm:items-stretch sm:justify-start">
+          <div class="hidden sm:ml-6 sm:block">
+            <div class="flex space-x-4 items-center">
+              <a
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                :class="[
+                  item.current ? ' text-orangePastel ' : 'text-white hover:text-orangePastel',
+                  'rounded-md px-3 py-2 text-sm font-medium'
+                ]"
+                :aria-current="item.current ? 'page' : undefined"
+                >{{ item.name }}</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <DisclosurePanel class="sm:hidden">
+      <div class="space-y-1 px-2 pb-3 pt-2">
+        <DisclosureButton
+          v-for="item in navigation"
+          :key="item.name"
+          as="a"
+          :href="item.href"
+          :class="[
+            item.current ? 'bg-gray-900 text-orangePastel ' : 'text-white hover:text-orangePastel',
+            'block rounded-md px-3 py-2 text-base font-medium'
+          ]"
+          :aria-current="item.current ? 'page' : undefined"
+          >{{ item.name }}</DisclosureButton
+        >
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
+</template>
+
+<script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+import logo from './icons/logo.vue'
+
+const navigation = [
+  { name: 'Beranda', href: '#', current: true },
+  { name: 'Sejarah Lontar', href: '#', current: false },
+  { name: 'Koleksi Lontar', href: '#', current: false },
+  { name: 'Tentang Kami', href: '#', current: false }
+]
+</script>
