c2f3a67cb35af1583c943b256f79255b579b72f6
evinsdesign <kelvinsaputra024@gmail.com>
2024-03-31 22:42:01 +0800

    fix: NavbarComponent

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,11 +1,11 @@
 <template>
   <Disclosure as="nav" class="bg-darkBlue" v-slot="{ open }">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div class="relative flex h-16 items-center justify-center">
-        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+      <div class="relative flex h-16 items-center justify-center md:justify-between">
+        <div class="absolute inset-y-0 left-0 flex items-center md:hidden">
           <!-- Mobile menu button-->
           <DisclosureButton
-            class="relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-gray-700 hover:text-orangePastel focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orangePastel"
+            class="relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-gray-700 hover:text-orangePastel focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orangePastel transition ease-in-out delay-300"
           >
             <span class="absolute -inset-0.5"></span>
             <span class="sr-only">Open main menu</span>
@@ -13,11 +13,11 @@
             <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
           </DisclosureButton>
         </div>
-        <div class="flex flex-shrink-0 items-center">
+        <div class="flex justify-center flex-shrink-0 items-center">
           <logo class="h-14 w-auto" />
         </div>
-        <div class="flex items-center justify-center sm:items-stretch sm:justify-start">
-          <div class="hidden sm:ml-6 sm:block">
+        <div class="flex items-center justify-center md:items-stretch md:justify-start">
+          <div class="hidden md:ml-6 md:block">
             <div class="flex space-x-4 items-center">
               <a
                 v-for="item in navigation"
@@ -35,7 +35,7 @@
         </div>
       </div>
     </div>
-    <DisclosurePanel class="sm:hidden">
+    <DisclosurePanel class="md:hidden">
       <div class="space-y-1 px-2 pb-3 pt-2">
         <DisclosureButton
           v-for="item in navigation"
@@ -44,7 +44,7 @@
           :href="item.href"
           :class="[
             item.current ? 'bg-gray-900 text-orangePastel ' : 'text-white hover:text-orangePastel',
-            'block rounded-md px-3 py-2 text-base font-medium'
+            'block rounded-md px-3 py-2 xxsm:text-sm sm:text-base font-medium '
           ]"
           :aria-current="item.current ? 'page' : undefined"
           >{{ item.name }}</DisclosureButton
